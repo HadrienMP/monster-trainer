5b200715d936322803dd5745f33fe7afefee1872
HadrienMP <public@hadrienmp.fr>
2017-06-27 20:22:55 +0200

    Fix first failing acceptance test

--- src/main/java/fr/tarcaye/monster_trainer/World.java
+++ src/main/java/fr/tarcaye/monster_trainer/World.java
@@ -1,15 +1,18 @@
 package fr.tarcaye.monster_trainer;
 
 public class World {
+    private Coordinates at;
+
     public World(int width, int height) {
 
     }
 
     public void place(Trainer trainer, Coordinates at) {
 
+        this.at = at;
     }
 
     public Coordinates whereIs(Trainer trainer) {
-        return null;
+        return at;
     }
 }
